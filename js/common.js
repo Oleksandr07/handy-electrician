b4da34b0d1233c54d2f48dfc47b912ad0955223a
Oleksandr07 <Oleksandr07@mail.ru>
2016-04-25 12:48:07 +0300

    Update

--- js/common.js
+++ js/common.js
@@ -26,9 +26,8 @@ $(function(){
 		theme: 'tooltipster-noir',
 	});
 
-	//------------------    menu scroll    -------------------------------
-
-	$('.top_menu li a.subNavBtn').on('click',function(){
+	/*--------------------------------          top-menu            ------------------------------------*/
+	$('.top-menu li a.subNavBtn').on('click',function(){
 		$('html,body').animate({scrollTop:$($(this).attr('href')).offset().top},800);
 		return false;
 	});
@@ -37,14 +36,13 @@ $(function(){
 		var s_top=$(document).scrollTop();
 		// var w_hgh=window.innerHeight;
 		// var b_hgh=$('body').outerHeight();
-		// var cur_sect=$('.top_menu li a.subNavBtn').eq(0);
-		// $('.top_menu li a.subNavBtn').each(function(){if((w_hgh/3+s_top)>$($(this).attr('href')).offset().top){cur_sect=$(this);}});
-		// if((s_top+w_hgh+500)>b_hgh){cur_sect=$('.top_menu li a.subNavBtn').last();}
-		// if(!cur_sect.hasClass('active')){$('.top_menu li a.subNavBtn.active').removeClass('active');cur_sect.addClass('active');}
-		if(s_top>0) {$('.top_menu').addClass('fixed');}
-		else{$('.top_menu').removeClass('fixed');}
+		// var cur_sect=$('.top-menu li a.subNavBtn').eq(0);
+		// $('.top-menu li a.subNavBtn').each(function(){if((w_hgh/3+s_top)>$($(this).attr('href')).offset().top){cur_sect=$(this);}});
+		// if((s_top+w_hgh+500)>b_hgh){cur_sect=$('.top-menu li a.subNavBtn').last();}
+		// if(!cur_sect.hasClass('active')){$('.top-menu li a.subNavBtn.active').removeClass('active');cur_sect.addClass('active');}
+		if(s_top>0) {$('.top-menu').addClass('fixed');}
+		else{$('.top-menu').removeClass('fixed');}
 	}
-
 	setNavCurrentSection();
 	$(window).on('load',function(){
 		setNavCurrentSection();
@@ -56,6 +54,7 @@ $(function(){
 		setNavCurrentSection();
 	});
 
+	/*--------------------------------          questions            ------------------------------------*/
 	$('.questions-title').click(function() {
 		var $parent = $(this).parents('.questions-box');
 		if (!$parent.hasClass('active')) {
@@ -70,13 +69,30 @@ $(function(){
 		}
 	});
 
+	/*--------------------------------          SVG like            ------------------------------------*/
+	function like(){
+		if ($(window).width() < 1200) {
+			$('#like').attr('x', '884');
+		}
+		else{
+			$('#like').attr('x', '970');
+		}
+	}
+	$(window).resize(function(event) {
+		like();
+	});
+	like()
+	
+
+	/*--------------------------------          video player            ------------------------------------*/
 	$('.video-1').mediaelementplayer({
-    	videoWidth: 940,
-    	videoHeight: 580,
+    	// videoWidth: 940,
+    	// videoHeight: 580,
+		features: ['progress','postroll']
 	});
 	$('.video-2').mediaelementplayer({
-		videoWidth: 570,
-    	videoHeight: 380,
+		// videoWidth: 570,
+  //   	videoHeight: 380,
 		features: ['progress','postroll']
 	});
 	$('.video-3').mediaelementplayer({
@@ -85,67 +101,59 @@ $(function(){
 		features: ['progress','postroll']
 	});
 
+	/*--------------------------------          slider            ------------------------------------*/
 	$('.certificates-slider').slick({
 		dots: false,
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
 		centerMode: true,
-		variableWidth: true
+		variableWidth: true,
+		// responsive: [
+		// 	{
+		// 		breakpoint: 1024,
+		// 			settings: {
+		// 			slidesToShow: 3,
+		// 			slidesToScroll: 3,
+		// 			infinite: true,
+		// 			dots: true
+		// 		}
+		// 	}
+		// ]
 	});
 
-	//------------------    form relevant    -------------------------------
-	$('.fancybox').click(function(){
-		$('#modal_window .form_title').text($(this).attr('data-title'));
-		$('#modal_window .a_button').text($(this).attr('data-button'));
-		$('#modal_window .form_name').val($(this).attr('data-pos'));
+	/*--------------------------------          lamp animation            ------------------------------------*/
+	var waypoints = $('.lamp').waypoint(function(direction){
+		$('#' + this.element.id).addClass('active')
+	},{
+		offset: 'bottom-in-view'
+	});
+	var waypoints = $('.lamp').waypoint(function(direction){
+		$('#' + this.element.id).removeClass('active')
+	},{
+		offset: '-20%'
+	});
+	var waypoints = $('.lamp').waypoint(function(direction){
+		$('#' + this.element.id).addClass('active')
+	},{
+		offset: '0%'
+	});
+	var waypoints = $('.lamp').waypoint(function(direction){
+		$('#' + this.element.id).removeClass('active')
+	},{
+		offset: '100%'
 	});
 
-	/* - - - - - - - - - - - - - -   Отправка формы  - - - - - - - - - - - - - - - - */
-	$('.send-button').click(function(){
-     var parentClass=$(this).attr('rel');
-	 var paramsFancy={
-	    'scrolling':0,
-	    'autoScale': true,
-	    'transitionIn': 'elastic',
-	    'transitionOut': 'elastic',
-	    'speedIn': 500,
-	    'speedOut': 300,
-	    'autoDimensions': true,
-	    'centerOnScroll': true,
-	    'href' : '#modal-success',
-	    'padding' : '0',
-	    'height' : 'auto',
-	    helpers: {
-	            overlay: {
-	              locked: false
-	            }
-	        }
-	    };
-	    validate=1;
-	    validate_msg='';
-	    form=$('#'+$(this).attr('data-rel'));
-	     jQuery.each(form.find('.validate'), function(key, value) {
-	        if($(this).val()==''){
-	            validate_msg+=$(this).attr('title')+'\n';validate=0;
-	            $(this).focus();
-	            $(this).addClass('red-input');
-	        }else{
-	            $(this).removeClass('red_input');
-	        }
-	    });
-	    if(validate==1){
-	        $.ajax({
-	            url: 'ajax.php',
-	            data: 'action=send-form&'+form.serialize(),
-	            success: function(data){
-	                $.fancybox.open(paramsFancy);
-	            }
-	        });
-	        
-	    }else{
-	        /*alert(validate_msg);*/
-	    } 
+	/*--------------------------------          snake animation            ------------------------------------*/
+	var waypoints = $('#snake').waypoint(function(direction){
+		$('#' + this.element.id).addClass('active')
+	},{
+		offset: '50%'
+	});
+	var waypoints = $('#snake').waypoint(function(direction){
+		$('#' + this.element.id).removeClass('active')
+	},{
+		offset: '100%'
 	});
 
 
